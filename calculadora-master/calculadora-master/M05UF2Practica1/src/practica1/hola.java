44418246c3581d8e6f92f95832252bffb802f0bf
geryale29 <adaw22gale@inslaferreria.cat>
2023-02-06 19:50:32 +0100

    Una vez más

---
+++ calculadora-master/calculadora-master/M05UF2Practica1/src/practica1/hola.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1;
+
+/**
+ *
+ * @author gerya
+ */
+public class hola {
+    public static void main(String[] args) {
+        System.out.println("Hola mireia");
+    }
+}
