324367761cae2d58117ec4a9eacd3cf903f5e881
geryale29 <adaw22gale@inslaferreria.cat>
2023-02-06 19:43:18 +0100

    Probando

--- calculadora-master/calculadora-master/M05UF2Practica1/src/practica1/Practica1.java
+++ calculadora-master/calculadora-master/M05UF2Practica1/src/practica1/Practica1.java
@@ -59,7 +59,7 @@ public class Practica1 {
                 comprobar = true;
                 switch (operacion) {
                     case "+":
-                        res = n2 + n2;
+                        res = n1 + n2;
                         break;
                     case "-":
                         res = n1 - n2;
